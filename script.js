1bb15d84a2b370b0123994c08a1c3af7e2eabb15
Jpiter25 <161729263+MiguelDelacruzCueva@users.noreply.github.com>
2026-02-06 16:24:08 -0500

    CONFIGURACION BASE

---
+++ script.js
@@ -0,0 +1,21 @@
+/**
+ * -----------------------------------------------
+ * PROYECTO : FINANZAS PERSONALES AUTOMATIZADO
+ * AUTOR : MiguelDelacruzCueva
+ * -----------------------------------------------
+ */
+const GLOBAL_CONFIG={
+    // el script no mirara correos anteriores a esta fecha
+    FECHA_MINIMA_BUSQUEDA: "2025/12/01",
+
+    //nombres de las hojas
+    HOJA_DATOS: "DATA",
+    HOJA_CONFIG: "CONFIG",
+
+    //etiqueta de gmail para no repetir el correo
+    GMAIL_LABEL: "PROCESADO_APP_FINANZAS",
+
+    //Datos fijo de yape por ejemplo
+    YAPE_CELULAR: "999999999"
+
+};
